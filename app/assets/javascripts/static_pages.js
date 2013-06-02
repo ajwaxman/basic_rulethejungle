60d33cb5d821e3d20d54dccc1c2a0e66d221008e
Adam Waxman <awaxman11@gmail.com>
2013-06-02 11:59:55 -0400

    Change to coming soon

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
 	$(window).load(function(){
 		//change section height to height of window
 		var height = $(window).height();
-		$(".section_background").height((height)+"px");
+		$(".homepage_background").height((height)+"px");
 	});
 	
 	// when window us resizes
 	$(window).resize(function(){
 		//change section height to height of window
 		var height = $(window).height();
-		$(".section_background").height((height)+"px");
+		$(".homepage_background").height((height)+"px");
 	});
 
 	console.log($(window).height())
